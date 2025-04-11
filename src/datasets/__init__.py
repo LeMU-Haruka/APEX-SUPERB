45d218866d79c33bf6fbe16aa03c99841e4178b0
LeMU-Haruka <jingranxie@gmail.com>
2025-04-11 16:20:54 +0800

    update dataset config

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
+import os
 from .base_dataset import BaseDataset, ChoiceDataset, HFDataset
 
 # BASE_TASK_CONFIG_LIST = []
@@ -62,64 +63,53 @@ datasets_map = {
 # 将DATASET_PATH_CONFIG按 上面的格式重新排列
 # 方便后续使用
 
+DATASET_ROOT = '/userhome/datasets/SGBench'
 
 DATASET_PATH_CONFIG = {
     ###  foundation tasks ###
     # Speech
-    'asr_librispeech': '/userhome/datasets/SGBench/librispeech',
-    'asr_commonvoice': '/userhome/datasets/SGBench/commonvoice17_1000',
-    'emotion': '/userhome/datasets/SGBench/ser',
-    'dialogue_ser': '/userhome/datasets/SGBench/dialogue_ser',
+    'asr_librispeech': os.path.join(DATASET_ROOT, 'librispeech'),
+    'asr_commonvoice': os.path.join(DATASET_ROOT, 'commonvoice17_1000'),
+    'emotion': os.path.join(DATASET_ROOT, 'ser'),
+    'dialogue_ser': os.path.join(DATASET_ROOT, 'dialogue_ser'),
     # sound
-    'sound_classification': '/userhome/datasets/SGBench/sound_classification',
-    'animal_classification': '/userhome/datasets/SGBench/animal_classification',
-
+    'sound_classification': os.path.join(DATASET_ROOT, 'sound_classification'),
+    'animal_classification': os.path.join(DATASET_ROOT, 'animal_classification'),
     ### Instruction tasks ###
     # multi-instruct
-    'text_instruct_asr': '/userhome/datasets/SGBench/text_instruct_asr',
-    'text_instruct_st': '/userhome/datasets/SGBench/text_instruct_st',
-
+    'text_instruct_asr': os.path.join(DATASET_ROOT, 'text_instruct_asr'),
+    'text_instruct_st': os.path.join(DATASET_ROOT, 'text_instruct_st'),
     # speech instruction
     # TODO
+
     # IFEval
-    'ifeval': '/userhome/datasets/SGBench/IFEval',
-
+    'ifeval': os.path.join(DATASET_ROOT, 'IFEval'),
     # Text Few shot instruction
-    'fewshot_gsm8k_1': '/userhome/datasets/SGBench/gsm8k_tts',
-    'fewshot_gsm8k_2': '/userhome/datasets/SGBench/gsm8k_tts',
-    'fewshot_gsm8k_4': '/userhome/datasets/SGBench/gsm8k_tts',
-    'fewshot_gsm8k_8': '/userhome/datasets/SGBench/gsm8k_tts',
-
+    'fewshot_gsm8k_1': os.path.join(DATASET_ROOT, 'gsm8k_tts'),
+    'fewshot_gsm8k_2': os.path.join(DATASET_ROOT, 'gsm8k_tts'),
+    'fewshot_gsm8k_4': os.path.join(DATASET_ROOT, 'gsm8k_tts'),
+    'fewshot_gsm8k_8': os.path.join(DATASET_ROOT, 'gsm8k_tts'),
     # Speech few shot instruction
     # TODO
-
     ### Input Adaptability tasks ###
     # Noisy robustness
-    'librispeech_noise': '/userhome/datasets/SGBench/librispeech_noise',
-
+    'librispeech_noise': os.path.join(DATASET_ROOT, 'librispeech_noise'),
     # Style speech adaptability
-    'librispeech_multispeaker': '/userhome/datasets/SGBench/librispeech_multispeaker',
-    'librispeech_emotion': '/userhome/datasets/SGBench/librispeech_emotion',
-    'librispeech_speed': '/userhome/datasets/SGBench/librispeech_speed',
-
+    'librispeech_multispeaker': os.path.join(DATASET_ROOT, 'librispeech_multispeaker'),
+    'librispeech_emotion': os.path.join(DATASET_ROOT, 'librispeech_emotion'),
+    'librispeech_speed': os.path.join(DATASET_ROOT, 'librispeech_speed'),
     ### Chat and Reasoning tasks ###
     # Knowledge
-    'mmlu': '/userhome/datasets/SGBench/mmlu_tts',
-
+    'mmlu': os.path.join(DATASET_ROOT, 'mmlu_tts'),
     # Math reasoning
-    'gsm8k': '/userhome/datasets/SGBench/gsm8k_tts',
-
+    'gsm8k': os.path.join(DATASET_ROOT, 'gsm8k_tts'),
     # content reasoning
-    'speaker_role': '/userhome/datasets/SGBench/speaker_role_tts',
-
+    'speaker_role': os.path.join(DATASET_ROOT, 'speaker_role_tts'),
     # audio reasoning
-    'mmau': '/userhome/datasets/SGBench/mmau',
-
+    'mmau': os.path.join(DATASET_ROOT, 'mmau'),
     # dialogue
-    'alpaca_eval': '/userhome/datasets/SGBench/alpacaeval_tts',
-    'empathy': '/userhome/datasets/SGBench/alpaca_eval_emotion',
-    'nutshell': '/userhome/datasets/SGBench/nutshell',
-    # Note: The key 'nutshell' from the original DATASET_PATH_CONFIG is omitted
-    # as it's not present in the datasets_map structure provided.
+    'alpaca_eval': os.path.join(DATASET_ROOT, 'alpacaeval_tts'),
+    'empathy': os.path.join(DATASET_ROOT, 'alpaca_eval_emotion'),
+    'nutshell': os.path.join(DATASET_ROOT, 'nutshell'),
 
 }
